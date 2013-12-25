dfcf2bc09f8f82f9570f423d6372458431319d5d
= <cschreib@orange.fr>
2013-12-25 23:04:27 +0100

    Added erase_if.

--- common/include/std_addon.hpp
+++ common/include/std_addon.hpp
@@ -4,12 +4,15 @@
 #include <memory>
 
 namespace std {
+    // For C++14 future support
+    // Construct an object inside an std::unique_ptr
     template<typename T, typename... Args>
     std::unique_ptr<T> make_unique(Args&&... args) {
         return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
     }
 }
 
+// Adapter to iterate over a range returned as std::pair<iterator>
 template<typename T>
 struct range_t {
     std::pair<T,T> p;
@@ -20,5 +23,11 @@ struct range_t {
 
 template<typename T>
 range_t<T> range(const std::pair<T,T>& p) { return range_t<T>(p); }
+
+// Shortcut for std::remove_if
+template<typename C, typename F>
+void erase_if(C& cont, F&& func) {
+    cont.erase(std::remove_if(cont.begin(), cont.end(), std::forward<F>(func)), cont.end());
+}
 
 #endif
