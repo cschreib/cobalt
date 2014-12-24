ea01dc6ff55251a1fb86b23ef469331eede192d1
= <cschreib@orange.fr>
2014-12-24 18:27:43 +0100

    config::shared_state is now working.

--- common-netcom/include/config_shared_state.hpp
+++ common-netcom/include/config_shared_state.hpp
@@ -2,7 +2,7 @@
 #define CONFIG_SHARED_STATE_HPP
 
 #include "shared_collection.hpp"
-#include "any.hpp"
+#include "netcom_base.hpp"
 
 namespace packet {
     NETCOM_PACKET(config_state) {
@@ -15,6 +15,8 @@ namespace packet {
     };
 
     NETCOM_PACKET(config_empty) {};
+
+    NETCOM_PACKET(config_clear) {};
 }
 
 namespace config {
@@ -23,6 +25,7 @@ namespace config {
             using full_packet   = packet::config_state;
             using add_packet    = packet::config_value_changed;
             using remove_packet = packet::config_empty;
+            using clear_packet  = packet::config_clear;
         };
 
         friend class shared_state_observer;
@@ -30,8 +33,26 @@ namespace config {
         shared_collection<shared_collection_traits> shared_;
 
     public :
-        shared_state(netcom_base& net, std::string name);
+        template<typename Netcom>
+        shared_state(Netcom& net, std::string name) :
+            shared_(net.template make_shared_collection<shared_collection_traits>(
+                std::move(name))) {
+            shared_.make_collection_packet([this](packet::config_state& st) {
+                std::ostringstream ss;
+                save(ss);
+                st.serialized = ss.str();
+            });
 
+            on_value_changed.connect([this](const std::string& name, const std::string& value) {
+                shared_.add_item(name, value);
+            });
+
+            shared_.connect();
+        }
+
+        void clear();
+
+        using state::on_value_changed;
         using state::parse;
         using state::parse_from_file;
         using state::save;
@@ -46,10 +67,36 @@ namespace config {
 
     class shared_state_observer : private state {
         shared_collection_observer<shared_state::shared_collection_traits> shared_;
+        scoped_connection_pool pool_;
+        actor_id_t aid_;
 
     public :
-        shared_state_observer(netcom_base& net, actor_id_t aid, const std::string& name);
+        template<typename Netcom>
+        shared_state_observer(Netcom& net, actor_id_t aid, const std::string& name) : aid_(aid) {
+            pool_ << net.send_request(aid, make_packet<request::get_shared_collection_id>(name),
+                [&,this](const netcom_base::request_answer_t<request::get_shared_collection_id>& msg) {
 
+                shared_ = net.template make_shared_collection_observer
+                    <shared_state::shared_collection_traits>(msg.answer.id);
+
+                shared_.on_received.connect([this](const packet::config_state& st) {
+                    std::istringstream ss(st.serialized);
+                    parse(ss);
+                });
+
+                shared_.on_add_item.connect([this](const packet::config_value_changed& val) {
+                    set_raw_value(val.name, val.value);
+                });
+
+                shared_.on_clear.connect([this](const packet::config_clear& val) {
+                    clear();
+                });
+
+                shared_.connect(aid_);
+            });
+        }
+
+        using state::on_value_changed;
         using state::save;
         using state::save_to_file;
         using state::get_value;
@@ -58,5 +105,9 @@ namespace config {
         using state::bind;
     };
 }
+
+#ifndef NO_AUTOGEN
+#include "autogen/packets/config_shared_state.hpp"
+#endif
 
 #endif
