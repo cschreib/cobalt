4b50ad0790db22d7c0f10f1eceb41fa7aa077a67
cschreib <cschreib@orange.fr>
2015-01-14 23:13:19 +0100

    Moved generator_base.hpp to its own folder.

--- generators/include/generator_base.hpp
+++ generators/include/generator_base.hpp
@@ -1,6 +1,8 @@
 #ifndef GENERATOR_BASE_HPP
 #define GENERATOR_BASE_HPP
 
+extern "C" bool generate_universe(const char* serialized_config, char** errmsg);
+
 extern "C" void free_error(char* errmsg) {
     delete[] errmsg;
 }
