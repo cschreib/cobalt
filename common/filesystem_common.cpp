7b96ce169b5f1e5615654d5cfd9fe52e4e0e652a
cschreib <cschreib@orange.fr>
2016-01-31 17:08:41 +0100

    Added file_to_packet and packet_to_file

--- common/filesystem_common.cpp
+++ common/filesystem_common.cpp
@@ -1,6 +1,7 @@
 #include "filesystem.hpp"
 #include "string.hpp"
 #include <fstream>
+#include <SFML/Network/Packet.hpp>
 
 namespace file {
     bool exists(const std::string& file) {
@@ -25,5 +26,28 @@ namespace file {
         std::vector<std::string> tree = string::split(file, "/");
         if (tree.size() == 1) return "./";
         return file.substr(0, file.size()-tree[tree.size()-1].size());
+    }
+
+
+    bool file_to_packet(const std::string& path, sf::Packet& p) {
+        std::ifstream file(path, std::ios::binary);
+        if (!file) return false;
+
+        file.seekg(0, std::ios_base::end);
+        std::size_t nbyte = file.tellg();
+        file.seekg(0, std::ios_base::beg);
+
+        std::vector<char> buffer(nbyte);
+        p.append(buffer.data(), nbyte);
+    }
+
+    bool packet_to_file(const sf::Packet& p, const std::string& path) {
+        std::ofstream file(path, std::ios::binary);
+        if (!file) return false;
+
+        file.write(tp.getData(), tp.getDataSize());
+
+        if (!file) return false;
+        else       return true;
     }
 }
