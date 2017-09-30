cc0ba7d85072d5535572bfc15d66d112ccf6afaa
cschreib <cschreib@orange.fr>
2017-09-30 23:18:29 +0200

    Made config::shared_state values type-safe and constrained

--- common-netcom/config_shared_state.cpp
+++ common-netcom/config_shared_state.cpp
@@ -1,8 +1,15 @@
 #include "config_shared_state.hpp"
+#include <string.hpp>
 
 namespace config {
+    const std::string meta_header = "__meta.";
+
+    bool typed_state::is_meta(const std::string& name) const {
+        return string::start_with(name, meta_header);
+    }
+
     void shared_state::clear() {
-        state::clear();
+        typed_state::clear();
         shared_.clear();
     }
 }
