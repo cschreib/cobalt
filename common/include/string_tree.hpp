7c7c313a1b6626982611ba08af05118341459458
Corentin Schreiber <cschreib@orange.fr>
2020-01-31 21:50:12 +0000

    Allow reaching branches

--- common/include/string_tree.hpp
+++ common/include/string_tree.hpp
@@ -68,7 +68,7 @@ namespace ctl {
         /// Branch of the tree : contains other nodes.
         struct branch : public node {
             branch(const std::string& name) : node(name, true) {}
-            sorted_vector<std::unique_ptr<node>, mem_var_comp(&node::name)> childs;
+            sorted_vector<std::unique_ptr<node>, mem_var_comp(&node::name)> children;
         };
 
         /// Leaf of the tree : contains data.
@@ -91,7 +91,7 @@ namespace ctl {
         *   \param name The name of the leaf to reach
         */
         const T* try_reach(const std::string& name) const {
-            const leaf* l = try_reach_(root_, name, 0);
+            const leaf* l = try_reach_<leaf>(root_, name, 0);
             if (l) {
                 return &l->data;
             } else {
@@ -108,14 +108,38 @@ namespace ctl {
         *   \param name The name of the leaf to reach
         */
         T& reach(const std::string& name) {
-            return reach_(root_, name, 0).data;
+            return reach_<leaf>(root_, name, 0).data;
+        }
+
+        /// Try to reach a branch of the tree by its key.
+        /** If the provided key is incompatible with the current
+        *   tree structure, i.e. it incorrectly assumes that a node
+        *   is a branch or a leaf, then no node is returned.
+        *   It also will not modify the tree, so it will not create
+        *   any node in order to reach it.
+        *   \param name The name of the branch to reach
+        */
+        const branch* try_reach_branch(const std::string& name) const {
+            return try_reach_<branch>(root_, name, 0);
+        }
+
+        /// Reach or create a branch of the tree by its key.
+        /** If the provided key is incompatible with the current
+        *   tree structure, i.e. it incorrectly assumes that a node
+        *   is a branch or a leaf, then an exception is raised.
+        *   If the branch or any of its parent do not exist, they
+        *   will be created.
+        *   \param name The name of the branch to reach
+        */
+        branch& reach_branch(const std::string& name) {
+            return reach_<branch>(root_, name, 0);
         }
 
         /// Removes all the elements from the tree.
         /** Only leaves the root branch.
         */
         void clear() {
-            root_.childs.clear();
+            root_.children.clear();
         }
 
         /// Reference to the root branch.
@@ -135,7 +159,8 @@ namespace ctl {
     private :
         branch root_;
 
-        const leaf* try_reach_(const branch& b, const std::string& name, std::size_t start) const {
+        template<typename U>
+        const U* try_reach_(const branch& b, const std::string& name, std::size_t start) const {
             std::size_t pos = name.find_first_of('.', start);
             std::string node_name;
             if (pos != std::string::npos) {
@@ -144,17 +169,19 @@ namespace ctl {
                 node_name = name.substr(start);
             }
 
-            auto iter = b.childs.find(node_name);
-            if (iter != b.childs.end()) {
-                if ((*iter)->is_branch) {
-                    if (pos != std::string::npos) {
-                        return try_reach_(static_cast<const branch&>(**iter), name, pos+1);
+            auto iter = b.children.find(node_name);
+            if (iter != b.children.end()) {
+                auto* node = iter->get();
+                if (pos == std::string::npos) {
+                    if ((std::is_same<U,branch>::value && node->is_branch) ||
+                        (std::is_same<U,leaf>::value && !node->is_branch)) {
+                        return static_cast<const U*>(node);
                     } else {
                         return nullptr;
                     }
                 } else {
-                    if (pos == std::string::npos) {
-                        return static_cast<const leaf*>(iter->get());
+                    if (node->is_branch) {
+                        return try_reach_<U>(static_cast<const branch&>(*node), name, pos+1);
                     } else {
                         return nullptr;
                     }
@@ -164,7 +191,8 @@ namespace ctl {
             }
         }
 
-        leaf& reach_(branch& b, const std::string& name, std::size_t start) const {
+        template<typename U>
+        U& reach_(branch& b, const std::string& name, std::size_t start) const {
             std::size_t pos = name.find_first_of('.', start);
             std::string node_name;
             if (pos != std::string::npos) {
@@ -173,28 +201,32 @@ namespace ctl {
                 node_name = name.substr(start);
             }
 
-            auto iter = b.childs.find(node_name);
-            if (iter != b.childs.end()) {
-                if ((*iter)->is_branch) {
-                    if (pos != std::string::npos) {
-                        return reach_(static_cast<branch&>(**iter), name, pos+1);
-                    } else {
+            auto iter = b.children.find(node_name);
+            if (iter != b.children.end()) {
+                auto& node = **iter;
+                if (pos == std::string::npos) {
+                    if (std::is_same<U,branch>::value && !node.is_branch) {
+                        throw expecting_branch_exception(name.substr(0, pos));
+                    }
+                    if (std::is_same<U,leaf>::value && node.is_branch) {
                         throw expecting_leaf_exception(name.substr(0, pos));
                     }
+
+                    return static_cast<U&>(node);
                 } else {
-                    if (pos == std::string::npos) {
-                        return static_cast<leaf&>(**iter);
+                    if (node.is_branch) {
+                        return reach_<U>(static_cast<branch&>(node), name, pos+1);
                     } else {
                         throw expecting_branch_exception(name.substr(0, pos));
                     }
                 }
             } else {
                 if (pos == std::string::npos) {
-                    auto iter = b.childs.insert(std::unique_ptr<node>(new leaf(node_name)));
-                    return static_cast<leaf&>(**iter);
+                    auto iter = b.children.insert(std::unique_ptr<node>(new U(node_name)));
+                    return static_cast<U&>(**iter);
                 } else {
-                    auto iter = b.childs.insert(std::unique_ptr<node>(new branch(node_name)));
-                    return reach_(static_cast<branch&>(**iter), name, pos+1);
+                    auto iter = b.children.insert(std::unique_ptr<node>(new branch(node_name)));
+                    return reach_<U>(static_cast<branch&>(**iter), name, pos+1);
                 }
             }
         }
